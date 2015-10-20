a72c58f5f3d79599a46618120d5054c901835d32
Katerina <katerinahillios@icloud.com>
2015-10-20 03:17:55 +0000

    Add answers to question detail view

--- core/views.py
+++ core/views.py
@@ -25,6 +25,14 @@ class QuestionDetailView(DetailView):
   model = Question
   template_name = 'question/question_detail.html'
 
+  def get_context_data (self, **kwargs):
+    context = super(QuestionDetailView, self).get_context_data(**kwargs)
+    question = Question.objects.get(id=self.kwargs['pk'])
+    answers = Answer.objects.filter(question=question)
+    context['answers'] = answers
+    return context
+
+
 class QuestionUpdateView(UpdateView):
   model = Question
   template_name = 'question/question_form.html'
@@ -39,10 +47,10 @@ class AnswerCreateView(CreateView):
   model = Answer
   template_name = 'answer/answer_form.html'
   fields = ['text']
-  
+
   def get_success_url(self):
     return self.object.question.get_absolute_url()
-  
+
   def form_valid(self,form):
     form.instance.user = self.request.user
     form.instance.question = Question.objects.get(id=self.kwargs['pk'])
